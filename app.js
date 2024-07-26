3f4d8e30043c7ab0366c60688a2103ebe4267b1d
darmawguna <komangagus642@gmail.com>
2024-07-26 11:00:19 +0700

    menambahkan fitur cors

--- app.js
+++ app.js
@@ -1,10 +1,38 @@
 import express from "express";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
+import cors from "cors"; 
 
 dotenv.config();
 
 const app = express();
+
+// Daftar asal (origin) yang diizinkan
+const allowedOrigins = [
+  // 'https://example.com', // Aplikasi Web
+  "http://localhost:5173", // Aplikasi Web saat pengembangan
+  // 'capacitor://localhost', // Aplikasi Mobile dengan Capacitor
+  // 'ionic://localhost', // Aplikasi Mobile dengan Ionic
+  // 'file://' // Aplikasi Mobile yang diakses dari file lokal
+];
+
+// Opsi CORS
+const corsOptions = {
+  origin: (origin, callback) => {
+    // Memeriksa apakah asal (origin) termasuk dalam daftar yang diizinkan
+    if (allowedOrigins.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: "GET,POST,PUT,DELETE",
+  allowedHeaders: "Content-Type,Authorization",
+  credentials: true,
+};
+
+// Gunakan middleware CORS dengan opsi yang disesuaikan
+app.use(cors(corsOptions));
 
 // Middleware
 app.use(bodyParser.json());
