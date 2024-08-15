33adc7af8e19406d4b9e76a2a14f46b15aee32ac
darmawguna <komangagus642@gmail.com>
2024-08-15 09:51:16 +0700

    update fix server dan app

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
-import cors from "cors"; 
+import cors from "cors";
 
 dotenv.config();
 
@@ -11,6 +11,10 @@ const app = express();
 const allowedOrigins = [
   // 'https://example.com', // Aplikasi Web
   "http://localhost:5173", // Aplikasi Web saat pengembangan
+  // TODO tambahkan alamat ip untuk laptop klien kgeo
+  "http://192.168.100.126:5173", // alamat IP laptop Nanda
+  "http://192.168.100.126", // alamat IP laptop klien
+  "https://idx.google.com/client-testing-1607078", // alamat Project IDX
   // 'capacitor://localhost', // Aplikasi Mobile dengan Capacitor
   // 'ionic://localhost', // Aplikasi Mobile dengan Ionic
   // 'file://' // Aplikasi Mobile yang diakses dari file lokal
@@ -42,11 +46,13 @@ import sensorRoutes from "./routes/sensorRoutes.js";
 import waterLevelRoutes from "./routes/waterlevelRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import alertRoutes from "./routes/alertRoutes.js";
-
+// TODO update routes dengan prefix /api
 app.use("/sensors", sensorRoutes);
-app.use("/waterlevels", waterLevelRoutes);
 app.use("/users", userRoutes);
 app.use("/alerts", alertRoutes);
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 // Error handling middleware (optional)
 app.use((err, req, res, next) => {
--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 import http from "http";
 import app from "./app.js";
-import socketConfig from "./config/socket.js";
 
 const server = http.createServer(app);
-const io = socketConfig(server);
 
 const PORT = process.env.PORT || 3000;
 
